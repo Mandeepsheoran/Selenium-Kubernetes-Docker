f27bc4b378b73f62bc11e9f9188cf24c5549c274
Mandeep Sheoran <Mandeep.Sheoran@igtsolutions.com>
2022-11-23 16:47:33 +0400

    adding selenium code

---
+++ src/test/java/com/selkubernetes/seleniumkubernetes/MainPageTest.java
@@ -0,0 +1,82 @@
+package com.selkubernetes.seleniumkubernetes;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.*;
+
+import static org.testng.Assert.*;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+public class MainPageTest {
+    private WebDriver driver;
+    private MainPage mainPage;
+   // public DesiredCapabilities capabilities;
+
+
+    @BeforeMethod
+    public void setUp(String browser) {
+        if(browser == "firefox") {
+            FirefoxOptions options = new FirefoxOptions();
+        } else if(browser == "chrome"){
+            ChromeOptions options = new ChromeOptions();
+        try {
+            driver = new RemoteWebDriver(new URL("http://192.168.64.2:32464"), options);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.jetbrains.com/");
+
+        mainPage = new MainPage(driver);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void search() {
+        mainPage.searchButton.click();
+
+        WebElement searchField = driver.findElement(By.cssSelector("[data-test='search-input']"));
+        searchField.sendKeys("Selenium");
+
+        WebElement submitButton = driver.findElement(By.cssSelector("button[data-test='full-search-button']"));
+        submitButton.click();
+
+        WebElement searchPageField = driver.findElement(By.cssSelector("input[data-test='search-input']"));
+        assertEquals(searchPageField.getAttribute("value"), "Selenium");
+    }
+
+    @Test
+    public void toolsMenu() {
+        mainPage.toolsMenu.click();
+
+        WebElement menuPopup = driver.findElement(By.cssSelector("div[data-test='main-submenu']"));
+        assertTrue(menuPopup.isDisplayed());
+    }
+
+    @Test
+    public void navigationToAllTools() {
+        mainPage.seeDeveloperToolsButton.click();
+        mainPage.findYourToolsButton.click();
+
+        WebElement productsList = driver.findElement(By.id("products-page"));
+        assertTrue(productsList.isDisplayed());
+        assertEquals(driver.getTitle(), "All Developer Tools and Products by JetBrains");
+    }
+}
